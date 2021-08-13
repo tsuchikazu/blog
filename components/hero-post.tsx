10fc36d3f7895ed11da6b84f5678314d8d685e64
tsuchikazu <tsuchikazu0317@gmail.com>
2021-08-13 17:31:10 +0900

    cover imageが存在するときだけ表示するようにした

--- components/hero-post.tsx
+++ components/hero-post.tsx
@@ -20,7 +20,7 @@ const HeroPost = ({
   return (
     <section>
       <div className="mb-8 md:mb-16">
-        <CoverImage title={title} src={coverImage} slug={slug} />
+        {coverImage && <CoverImage title={title} src={coverImage} slug={slug} />}
       </div>
       <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
         <div>
