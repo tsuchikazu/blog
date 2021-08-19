e66135b45ea6d045319ab334179faedcbd9872bc
tsuchikazu <tsuchikazu0317@gmail.com>
2021-08-19 14:56:52 +0900

    スタイルを微調整

--- components/post-header.tsx
+++ components/post-header.tsx
@@ -12,10 +12,12 @@ const PostHeader = ({ title, coverImage, date }: Props) => {
   return (
     <>
       <PostTitle>{title}</PostTitle>
-      <div className="mb-8 md:mb-16 sm:mx-0">
-        {coverImage && <CoverImage title={title} src={coverImage} />}
-      </div>
-      <div className="max-w-2xl mx-auto">
+      {coverImage &&
+        <div className="mb-8 md:mb-16 sm:mx-0 flex justify-center">
+          <CoverImage title={title} src={coverImage} />
+        </div>
+      }
+      <div className="max-w-5xl mx-auto">
         <div className="mb-6 text-lg">
           <DateFormatter dateString={date} />
         </div>
