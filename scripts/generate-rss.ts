772da744075aa4bd2091cc39c628e8923bb307f7
tsuchikazu <tsuchikazu0317@gmail.com>
2021-09-13 17:47:52 +0900

    feed.xmlを自動生成するようにした

---
+++ scripts/generate-rss.ts
@@ -0,0 +1,36 @@
+import { getAllPosts } from '../lib/api'
+import RSS from 'rss'
+import * as fs from 'fs'
+import stripMarkdown from '../lib/stripMarkdown'
+
+async function main() {
+  const feed = new RSS({
+    title: 'tsuchikazu blog',
+    site_url: 'https://tsuchikazu.net',
+    feed_url: 'https://tsuchikazu.net/feed.xml'
+  })
+
+  await Promise.all(
+    getAllPosts([
+      'slug',
+      'title',
+      'date',
+      'content',
+      'categories',
+    ]).map(async (post) => {
+      const content = await stripMarkdown(post.content || '')
+      feed.item({
+        title: post.title,
+        url: `/${post.slug}`,
+        date: post.date,
+        description: content.substring(0, 200),
+        // @ts-ignore
+        categories: post.categories,
+      })
+    })
+  )
+
+  fs.writeFileSync('./public/feed.xml', feed.xml({ indent: true }))
+}
+
+main()
