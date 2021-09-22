f9a1a02ccd9d7624312acc1a4c0559c2d1dd5d37
tsuchikazu <tsuchikazu0317@gmail.com>
2021-09-22 17:27:22 +0900

    google AdSenseのコードをコピペ

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,6 +22,13 @@ export default class MyDocument extends Document {
           `,
             }}
           />
+
+          {/* Google Adsense */}
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7218976215807284"
+            crossOrigin="anonymous"
+          />
         </Head>
         <body>
           <Main />
